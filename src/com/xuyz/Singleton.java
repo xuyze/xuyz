c15432911dd4d52d7e85b62a3e68a11de59f8b60
xuyz <xuyze@sina.com>
2017-03-31 10:45:38 +0800

    test

---
+++ src/com/xuyz/Singleton.java
@@ -0,0 +1,24 @@
+package com.xuyz;
+
+/**
+ * Created by wang on 2017/3/20.
+ */
+
+/**
+ * 单例模式
+ * 应用场合：有些对象只需要一个就足够了
+ * 作用：保证整个应用程序中某个实例有且只有一个
+ */
+public class Singleton {
+
+    private Singleton(){
+
+    }
+
+    private static Singleton instance = new Singleton();
+
+    public static Singleton getInstance(){
+        return instance;
+    }
+
+}
---
+++ src/com/xuyz/Test.java
@@ -0,0 +1,7 @@
+package com.xuyz;
+
+/**
+ * Created by wang on 2017/3/20.
+ */
+public class Test {
+}
