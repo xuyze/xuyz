2f5e5e53eef442361af0635aa0e69c96ea103234
xuyz <12510705@qq.com>
2017-09-10 21:56:08 +0800

    1

--- src/com/xuyz/singleton/Singleton.java
+++ src/com/xuyz/singleton/Singleton.java
@@ -3,22 +3,21 @@ package com.xuyz.singleton;
 /**
  * Created by wang on 2017/3/20.
  */
-
-/**
- * 单例模式
- * 应用场合：有些对象只需要一个就足够了
- * 作用：保证整个应用程序中某个实例有且只有一个
- */
 public class Singleton {
 
     private Singleton(){
-
     }
 
-    private static Singleton instance = new Singleton();
+    private static volatile Singleton instance=null;
 
     public static Singleton getInstance(){
+        if(instance==null){
+            synchronized (Singleton.class) {
+                if(instance==null) {
+                    instance = new Singleton();
+                }
+            }
+        }
         return instance;
     }
-
 }
