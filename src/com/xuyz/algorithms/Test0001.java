93d3bcaf6e93c89982f40bc6641932d26a39de99
xuyz <12510705@qq.com>
2018-08-29 23:36:49 +0800

    测试

---
+++ src/com/xuyz/algorithms/Test0001.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: Test0001
+ * Author:   xuyz
+ * Date:     2018/8/19 22:10
+ * Description: 测试
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.xuyz.algorithms;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈测试〉
+ *
+ * @author xuyz
+ * @create 2018/8/19
+ * @since 1.0.0
+ */
+public class Test0001 {
+}
