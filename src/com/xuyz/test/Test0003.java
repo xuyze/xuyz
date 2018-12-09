9bb7526fd87df8f6f5aaa6fefec7053f1f333698
xuyz <12510705@qq.com>
2018-12-09 21:23:03 +0800

    1

---
+++ src/com/xuyz/test/Test0003.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (C), 2015-2018, XXX有限公司
+ * FileName: Test0003
+ * Author:   xuyz
+ * Date:     2018/12/5 23:47
+ * Description: 类加载器调试
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.xuyz.test;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈类加载器调试〉
+ *
+ * @author xuyz
+ * @create 2018/12/5
+ * @since 1.0.0
+ */
+public class Test0003 {
+
+    public static void main(String[] args) {
+        ClassLoader cl = Test0003.class.getClassLoader();
+        // AppClassLoader的父加载器是ExtClassLoader
+        System.out.println("ClassLoader is:"+cl.toString());
+        System.out.println("ClassLoader\'s parent is:"+cl.getParent().toString());
+        //System.out.println("ClassLoader\'s grand father is:"+cl.getParent().getParent().toString());
+    }
+}
