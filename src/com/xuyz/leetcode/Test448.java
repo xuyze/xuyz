bb8f418fa9cfab68352a10f4de8977a287d62dd2
xuyz <12510705@qq.com>
2019-03-10 15:05:53 +0800

    1

--- src/com/xuyz/leetcode/Test448.java
+++ src/com/xuyz/leetcode/Test448.java
@@ -10,7 +10,10 @@
  */
 package com.xuyz.leetcode;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import static java.lang.Math.abs;
 
 /**
  * 〈一句话功能简述〉<br> 
@@ -22,8 +25,21 @@ import java.util.List;
  */
 public class Test448 {
 
-    public List<Integer> findDisappearedNumbers(int[] nums) {
+    public static List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> result = new ArrayList<>();
+        for(int i= 0;i<nums.length;i++){
+            nums[abs(nums[i])-1] = -abs(nums[abs(nums[i])-1]);
+        }
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                result.add(i+1);
+            }
+        }
+        return result;
+    }
 
-        return null;
+    public static void main(String[] args) {
+        int[] test = new int[]{4,3,2,7,8,2,3,1};
+        System.out.println(findDisappearedNumbers(test));
     }
 }
