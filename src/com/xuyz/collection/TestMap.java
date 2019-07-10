a51384606923b14b55fa900ef29fb88c466262db
xuyz <12510705@qq.com>
2019-07-11 00:25:19 +0800

    HashMap源码学习

---
+++ src/com/xuyz/collection/TestMap.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: TestMap
+ * Author:   xuyz
+ * Date:     2019/7/10 23:58
+ * Description: HashMap原理学习
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.xuyz.collection;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈HashMap原理学习〉
+ *
+ * @author xuyz
+ * @date 2019/7/10
+ * @since 1.0.0
+ */
+public class TestMap {
+
+    public static void main(String[] args) {
+        Map<String,Object> map = new HashMap<>();
+        Hashtable<String,Object> hashtable = new Hashtable<>();
+        LinkedHashMap linkedHashMap = new LinkedHashMap();
+    }
+}
